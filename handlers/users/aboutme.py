9c88efb926175a6b2f54d1e8d4892dc4d940bcfc
Muhammadali0204 <mamadjonovmuhammadali7@gmail.com>
2023-02-12 20:35:40 +0500

    aboutme

--- handlers/users/aboutme.py
+++ handlers/users/aboutme.py
@@ -10,10 +10,10 @@ async def men(msg : types.Message):
         await msg.answer("<b>Iltimos /start ni bosing va ism familiyangizni kiriting!</b>", reply_markup=menu.menu)
         return
     answer = f"<b>Sizning ismingiz : {user[1]}</b>\n\n<i>*O`zgartirish uchun 🛠Sozlamalar/♻️Ismni tahrirlash bo'limiga o'ting.</i>"
-    answer += f"<b>Qolgan limitlaringiz : </b>\n\n"
-    answer += f"<b>📕Oddiy test, <i>manual</i> </b><i>{foydalanuvchi_limitlari_oddiy[0]} ta\n</i>"
-    answer += f"<b>📘Oddiy test, <i>avto</i> </b><i>{foydalanuvchi_limitlari_oddiy[1]} ta\n</i>"
-    answer += f"<b>📚Blok test, <i>manual</i> </b><i>{foydalanuvchi_limitlari_blok[0]} ta\n</i>"
-    answer += f"<b>📚Blok test, <i>avto</i> </b><i>{foydalanuvchi_limitlari_blok[1]} ta\n\n</i>"
-    answer += f"<b>Siz 🏆Premium foydalanuvchi emassiz"
+    # answer += f"<b>Qolgan limitlaringiz : </b>\n\n"
+    # answer += f"<b>📕Oddiy test, <i>manual</i> </b><i>{foydalanuvchi_limitlari_oddiy[0]} ta\n</i>"
+    # answer += f"<b>📘Oddiy test, <i>avto</i> </b><i>{foydalanuvchi_limitlari_oddiy[1]} ta\n</i>"
+    # answer += f"<b>📚Blok test, <i>manual</i> </b><i>{foydalanuvchi_limitlari_blok[0]} ta\n</i>"
+    # answer += f"<b>📚Blok test, <i>avto</i> </b><i>{foydalanuvchi_limitlari_blok[1]} ta\n\n</i>"
+    # answer += f"<b>Siz 🏆Premium foydalanuvchi emassiz"
     await msg.answer(text=answer)
